bb13f837630c0ca1060f7d81aa7aaec4fe4f24ed
Kamsnyc <kamel.singh1999@gmail.com>
2023-12-31 19:06:41 -0500

    added projects sidebar

--- app/components/ProjectCard.tsx
+++ app/components/ProjectCard.tsx
@@ -3,6 +3,18 @@ import { Badge } from '@/components/ui/badge';
 import { BiLinkExternal } from 'react-icons/bi';
 import Link from 'next/link';
 import { Button } from '@/components/ui/button';
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet"
+import { ScrollArea } from "@/components/ui/scroll-area"
+
+import Image from 'next/image';
+import { useState } from 'react';
 
 
 type Project = {
@@ -37,7 +49,7 @@ function ProjectCard({ projects }: ProjectCardProps) {
               </div>
               {/* RIGHT SIDE CONTAINER */}
               <div className="h-full w-full md:w-1/2 p-4 lg:p-12 ">
-                <h1 className="text-[24px] font-bold flex items-center gap-4">
+                <h1 className="text-[24px] font-bold flex items-center gap-4 mt-4">
                   {project.title}
                   <span>
                     {/* OPEN LINK */}
@@ -48,11 +60,45 @@ function ProjectCard({ projects }: ProjectCardProps) {
                     </div>
                   </span>
                 </h1>
-                <p className="text-[--dark-gray] text-[18px] leading-6 my-[24px]">
+                <p className="text-[--dark-gray] text-[18px] leading-6 my-[8px]">
                   {project.context}
                 </p>
-                {/* BADGES */}
-                <div className="grid grid-cols-4 grid-row-3 gap-x-6 gap-y-2">
+                {/* VIEW PROJECT IN DETAILS */}
+                <div className="">
+                <Sheet>
+                <SheetTrigger id="container" className='h-full w-full rounded-xl flex justify-end'>
+                    <Button className="bg-[#1821F8] uppercase w-[100px]">Info</Button>
+                </SheetTrigger>
+                 {/* SIDE OPEN PROJECT PAGE */}
+        <SheetContent className='w-full px-4'>
+          <ScrollArea className='h-full w-full '>
+            <SheetHeader>
+              <SheetTitle className='text-white text-right text-xs hover:text-[#1821F8] hover:scale-105 duration-300 ease-in pr-4 font-normal'><Link href={'/work'}>back to all projects</Link></SheetTitle>
+              {/* BORDER */}
+              <div className="border-b border-color py-2 px-2"></div>
+  
+              {/* SUBTITLE (SHORT DESCRIPTION) */}
+              <h1 className='font-bold text-[24px] text-[#1821F8]'>{project.title}</h1>
+              <p className='link-color text-[14px] leading-5'>{project.context}</p>
+  
+              {/* COVER IMAGE */}
+              <Image src={project.image} alt={`${project.alt}`} width={450} height={100} className='max-h-[250px] w-full rounded-2xl px-2 object-fill' />
+  
+              <SheetDescription className="h-full text-left px-2">
+                <div className="overflow-y-auto h-full">
+                  {/* WEBSITE */}
+                  <p className='text-base text-center cursoyr-pointer duration-300 ease-in-out hover:text-[#1821F8]'>{project.projectLink}</p>
+                  {/* ABOUT */}
+                  <h2 className='text-white text-base py-2 font-light'>About</h2>
+                  <p className='text-[14px] hover:text-black'>{project.context}</p>
+  
+                  {/* BORDER */ }
+                  <div className="border-b border-color py-2"></div>
+  
+                  {/* TECHNOLOGY */ }
+                  <h2 className='text-[#1821F8] text-base py-2 font-light'>Technologies</h2>
+                  {/* BADGES */}
+                <div className="grid grid-cols-4 grid-row-3 gap-x-6">
                   {project.tags.map((item, index) => (
                     <div key={index}>
                       <Badge
@@ -64,11 +110,21 @@ function ProjectCard({ projects }: ProjectCardProps) {
                     </div>
                   ))}
                 </div>
-                {/* VIEW PROJECT IN DETAILS */}
-                <div className="mt-5">
-                  <Link href={`/${project._id}`}>
-                    <Button className="">View More</Button>
-                  </Link>
+                  {/* BORDER */ }
+                  <div className="border-b border-color py-2"></div>
+  
+                </div>
+              </SheetDescription>
+  
+              {/* VIEW WEBSITE */}
+              <div className="flex flex-col md:flex-row w-full gap-8 pt-4 px-4">
+                <Button className='w-full md:w-auto text-black' variant="outline"><Link href={`${project.projectLink}`}>View Website</Link></Button>
+                <Button className='w-full md:w-auto text-black' variant="outline"><Link href={`${project.projectLink}`}>Github</Link></Button>
+              </div>
+            </SheetHeader>
+          </ScrollArea>
+        </SheetContent>
+      </Sheet>
                 </div>
               </div>
             </div>
